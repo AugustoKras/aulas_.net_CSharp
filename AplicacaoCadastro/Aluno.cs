ad62c0871059e611f8379c234b2a729cc3e2ef80
Augusto Kraszczuk <augusto.kraszczuk@gmail.com>
2021-02-17 10:35:18 -0300

    Aula_AplicacaoCadastro_01

---
+++ AplicacaoCadastro/Aluno.cs
@@ -0,0 +1,9 @@
+namespace AplicacaoCadastro
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; }
+
+        public decimal Nota { get; set; }
+    }
+}
---
+++ AplicacaoCadastro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AplicacaoCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "x")
+            {
+                
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+                    case "3":
+                        //TODO: calcular media geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opcao desejada");
+            Console.WriteLine("1 - Inserir novo Aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular media geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
